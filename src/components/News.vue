831a88d33a42339662070ffc187842e845db76ab
arivin <btc022003@gmail.com>
2018-06-20 15:54:13 +0800

    add actions

--- src/components/News.vue
+++ src/components/News.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="news">
-    <h5>当前计数值为:{{$store.state.count}}</h5>
+    <h5>当前计数值为:{{count}}</h5>
     <button @click="$store.commit('add')">加一</button>
+    <button @click="$store.commit('add', { step: 2 })">加二</button>
+    <button @click="$store.dispatch('addActionAsync', { step: 3 })">异步加3</button>
     <ul>
       <li v-for="item in news" @click="clickHandle(item.id)" :key="item.id">{{item.title}}</li>
     </ul>
   </div>
 </template>
 <script>
+import { mapState } from 'vuex';
+
 export default {
   data() {
     return {
@@ -21,6 +25,9 @@ export default {
   },
   created() {
     console.log(this.$route);
+  },
+  computed: {
+    ...mapState(['count']),
   },
   methods: {
     clickHandle(id) {
--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import Vuex from 'vuex';
 import App from './App';
 import router from './router';
-import Vuex from 'vuex';
 
 Vue.use(Vuex);
 const store = new Vuex.Store({
@@ -11,8 +11,20 @@ const store = new Vuex.Store({
     count: 1,
   },
   mutations: {
-    add(state) {
-      state.count += 1;
+    add(state, payload) {
+      console.log(payload);
+      if (payload) {
+        state.count += payload.step;
+      } else {
+        state.count += 1;
+      }
+    },
+  },
+  actions: {
+    addActionAsync({ commit }, payload) {
+      setTimeout(() => {
+        commit('add', payload);
+      }, 1000);
     },
   },
 });
