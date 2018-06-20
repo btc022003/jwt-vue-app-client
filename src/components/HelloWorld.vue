b95113705862541ad17ab556596418fc5f9f9c86
arivin <btc022003@gmail.com>
2018-06-20 15:22:03 +0800

    pass params from router

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,6 +2,7 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
+    <router-link :to="{name: 'News', query: { type: 'current' }}">News</router-link>
     <ul>
       <li>
         <a
---
+++ src/components/NewsDetail.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="detail">
+    <h5>当前的参数为{{id}}</h5>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      id: ''
+    };
+  },
+  created() {
+    this.id = this.$route.query.id;
+    console.log(this.$route.query);
+  },
+}
+</script>
+
